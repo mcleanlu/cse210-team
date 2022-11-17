ead027622d55767b736aa865de5d7891d80c22e5
coderkearns <coder.kearns@gmail.com>
2022-11-17 09:03:47 -0600

    remove files

--- W03-Teach/jumper.py
+++
@@ -1,7 +0,0 @@
-class Jumper:
-    def __init__(self):
-        self.strings_left = 5
-
-    def guess_incorrect(self):
-        """Subtracts 1 string for guessing incorrectly"""
-        self.strings_left -= 1
